87d5e39d9cb1aa388d91a7da7629ae9a1b7517f8
Indy <ivo.indergand@uzh.ch>
2018-08-30 05:40:48 +0200

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+module.exports = Phrase;
 
+//Adds reverse to all strings
 String.prototype.reverse = function(){
-    return Array.from(string).reverse().join("")
+    return Array.from(this).reverse().join("")
 }
 
 function Phrase(content) {
@@ -8,14 +10,21 @@ function Phrase(content) {
 
     // Returns content processed for palindrome testing.
     this.processedContent = function processedContent(){
-        return this.content.toLowerCase();
+        return this.letters().toLowerCase();
     }
+
+    //returns letters in the content
+    this.letters = function letters() {
+    return Array.from(this.content).filter(c => c.match(/[a-z]/i)).join("");
+  }    
 
     // Returns true if the phrase is a palindrome, false otherwise.
     this.palindrome = function palindrome(){
     return this.processedContent() === this.processedContent().reverse();
 }
 
+
+/*
 function TranslatedPhrase(content,translation) {
     this.content = content;
     this.translation = translation;
@@ -26,4 +35,5 @@ function TranslatedPhrase(content,translation) {
   }
     }
 }
-TranslatedPhrase.prototype = new TranslatedPhrase();
+TranslatedPhrase.prototype = new TranslatedPhrase(); 
+*/}
